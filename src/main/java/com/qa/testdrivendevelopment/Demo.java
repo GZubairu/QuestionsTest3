f4cdc77806f129d9776ab0740726cca643927b3f
Gabriel <gabriel.zubairu@academytrainee.com>
2020-04-24 15:10:16 +0100

    TDD3

---
+++ src/main/java/com/qa/testdrivendevelopment/Demo.java
@@ -0,0 +1,15 @@
+package com.qa.testdrivendevelopment;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		Questions q = new Questions();
+
+//		q.oddNum("1,3,5");
+		q.longString("hello", "ovals");
+	
+	}
+	
+
+}
